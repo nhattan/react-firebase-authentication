31ea0bc69b47a20594eef542361d0c17f46e0da2
Tan <nhattan2012@gmail.com>
2018-09-12 16:42:01 +0800

    update account page

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import AuthUserContext from './AuthUserContext';
+import { PasswordForgetForm } from './PasswordForget';
+import PasswordChangeForm from './PasswordChange';
+
 const AccountPage = () =>
-  <div>
-    <h1>Account Page</h1>
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => authUser
+      ?
+        <div>
+          <h1>Account: {authUser.email}</h1>
+          <PasswordForgetForm />
+          <PasswordChangeForm />
+        </div>
+      : <p>Please sign in first</p>
+    }
+  </AuthUserContext.Consumer>
 
 export default AccountPage;
